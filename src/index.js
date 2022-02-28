4bd7e865dd2bac5466368ade34c417a353c772f1
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-02-28 10:15:33 +0200

    update code

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+import reducer from "./components/store/reducer";
+import { Provider } from "react-redux";
+
+const myStore = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
