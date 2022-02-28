3145b5fe18e3457cb8832bdedce8a884b1a24085
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-02-28 12:43:37 +0200

    added filtering

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import classes from "./TodoList.module.css";
 import * as actionTypes from "../store/actions"
+
 
 const TodoList = () => {
   const notes = useSelector((state) => state.notes);
   const dispatch = useDispatch();
+  const [filteredValue, setFilteredValue] = useState();
+  const [filterList, setFilterList] = useState(notes);
 
   const removeHandler = (id) => {
     dispatch({
@@ -21,10 +25,35 @@ const TodoList = () => {
     });
   };
 
+  useEffect(() => {
+    if (filteredValue === "true") {
+      setFilterList(
+        notes.filter((item) => item.done === !!filteredValue)
+      );
+    }
+    else if (filteredValue === "false") {
+      setFilterList(
+        notes.filter((item) => item.done !== !!filteredValue)
+      );
+    }
+    else {
+      setFilterList(notes);
+    }
+  }, [filteredValue, notes]);
+
+  const filterHandler = (e) => {
+    setFilteredValue(e.target.value);
+  }
+
   return (
     <div className={classes.todos}>
       <h1>Notes:</h1>
-      {notes.map((note) => {
+      <select name="done" defaultValue="all" onChange={filterHandler}>
+        <option value="true">Done</option>
+        <option value="false">Not done</option>
+        <option value="all">All</option>
+      </select>
+      {filterList.map((note) => {
         return (
           <div
             onClick={() => doneHandler(note.id)}
